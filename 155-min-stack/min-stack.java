fb8bf6b78d347314652776bcb9610ccaf5b11610
Sairam-Innamuri <vtu24835@veltech.edu.in>
2026-04-08 10:57:48 +0530

    Time: 5 ms (85.46%) | Memory: 47.2 MB (36.51%) - LeetSync

---
+++ 155-min-stack/min-stack.java
@@ -0,0 +1,24 @@
+class MinStack {
+    private Stack<int[]> st;
+    public MinStack() {
+        st = new Stack<>();
+    }
+    public void push(int val) {
+        if (st.isEmpty()) {
+            st.push(new int[]{val, val});
+        } else {
+            int currentMin = Math.min(val, st.peek()[1]);
+            st.push(new int[]{val, currentMin});
+        }
+    }
+    public void pop() {
+        st.pop();
+    }
+
+    public int top() {
+        return st.peek()[0];   
+    }
+    public int getMin() {
+        return st.peek()[1];   
+    }
+}
